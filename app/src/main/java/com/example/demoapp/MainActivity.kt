4d895bb3176661fc7ac74534653025d5e20ed5db
yudiz-prakash <prakash.parmar@yudiz.com>
2023-04-28 18:14:52 +0530

    Custom Demo Half

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -8,19 +8,25 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     lateinit var  btnRegistartion : Button
     lateinit var  btnProfile : Button
+    lateinit var btnCutom :  Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnRegistartion = findViewById(R.id.btnRegistration)
         btnProfile = findViewById(R.id.btnProfile)
-
+        btnCutom = findViewById(R.id.btnCustom)
         btnRegistartion.setOnClickListener {
             var i = Intent(this,Registration::class.java)
             startActivity(i)
         }
         btnProfile.setOnClickListener {
             var intent = Intent(this,Profile::class.java)
+            startActivity(intent)
+        }
+
+        btnCutom.setOnClickListener {
+            var intent = Intent(this,Custom_Button_Demo::class.java)
             startActivity(intent)
         }
     }
