b26f9659426dbec6dfd76ad0ba48c8ca72ab741f
yudiz-prakash <prakash.parmar@yudiz.com>
2023-05-01 14:31:11 +0530

    Layout Task

---
+++ app/src/main/java/com/example/demoapp/ViewDemo.kt
@@ -0,0 +1,18 @@
+package com.example.demoapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.demoapp.R
+
+class ViewDemo : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
