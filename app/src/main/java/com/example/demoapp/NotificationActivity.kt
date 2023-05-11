9887108108dba91d565373c394122a9b8f50a0ff
yudiz-prakash <prakash.parmar@yudiz.com>
2023-05-11 18:39:23 +0530

    Multiple Layout Demo Revision2

--- app/src/main/java/com/example/demoapp/NotificationActivity.kt
+++ app/src/main/java/com/example/demoapp/NotificationActivity.kt
@@ -1,86 +1,169 @@
 package com.example.demoapp
 
-import android.annotation.SuppressLint
-import android.app.LauncherActivity
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+
+import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.net.Uri.Builder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
-import java.nio.channels.Channel
 
 
 class NotificationActivity : AppCompatActivity() {
 
-    private lateinit var btnNormalNotCol: Button
-    private lateinit var btnNormalNotExap: Button
-    private lateinit var notificationManager: NotificationManager
+    private lateinit var btnNormal: Button
+    private lateinit var btnFullScreen: Button
+    private lateinit var btnFullCustom: Button
+    private lateinit var channel: NotificationChannel
+    private var channelIDNormalNot = "normalNotification"
+    private var channelIDFullSNot= "fullSCreeNotification"
+    private var channelIDFullSCNot = "fullCustomNotification"
 
-        private lateinit var builder : NotificationCompat.Builder
-    private var channelId = "com.example.demoapp"
-    private var description1 = "First Notification"
+    private lateinit var builder: NotificationCompat.Builder
+    private lateinit var notificationManager: NotificationManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        btnNormalNotCol = findViewById(R.id.btnNormalNotColl)
-        btnNormalNotExap = findViewById(R.id.btnNormalNotExpand)
-
+        btnNormal = findViewById(R.id.btnNormalNotification)
+        btnFullScreen = findViewById(R.id.btnFullScreenNotification)
+        btnFullCustom = findViewById(R.id.btnFullYCustom)
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        btnNormalNotCol.setOnClickListener {
-            val intent = Intent(this, Notification::class.java).apply {
-                action
-            }
-            val pendingIntent =
-                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        btnNormal.setOnClickListener {
+            normalNotification()
+        }
 
-            val largeIcon = BitmapFactory.decodeResource(resources,R.drawable.img)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                builder = NotificationCompat.Builder(this, channelId)
-                    .setSmallIcon(R.drawable.ic_home_24)
-                    .setContentTitle("Alert")
-                    .setContentText("Alert from the Demo app")
-                    .setLargeIcon(largeIcon)
-                    .setStyle(NotificationCompat.BigPictureStyle()
-                        .bigPicture(largeIcon)
-                        .bigLargeIcon(null)
-                       )
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true)
-                val channel = NotificationChannel(
-                    channelId, getString(R.string.app_name),
-                    NotificationManager.IMPORTANCE_MIN
-                ).apply {
-                    description = description1
-                }
-                notificationManager.createNotificationChannel(channel)
-            }
-//             else {
-//                 builder = NotificationCompat.Builder(this, channelId)
-//                    .setSmallIcon(R.drawable.ic_home_24)
-//                    .setContentTitle("Alert")
-//                    .setContentText("Alert from the Demo app")
-//                    .setContentIntent(pendingIntent)
-//
-//
-//            }
-            notificationManager.notify(123, builder.build())
+        btnFullScreen.setOnClickListener {
+            fullScreenNotification()
+        }
 
+        btnFullCustom.setOnClickListener {
+            fullCustmoNotification()
         }
 
 
     }
 
+//    private fun createChannelGroup() {
+//        val groupID = "my_first_group"
+//        val groupName = getString(R.string.app_name)
+//        notificationManager.createNotificationChannelGroup(NotificationChannelGroup(groupID,groupName))
+//    }
+
+    private fun fullCustmoNotification() {
+        val fullyCustomNotification = Intent(this, MainActivity::class.java)
+        val fullyCustomNotificationPending =
+            PendingIntent.getActivity(this, 3, fullyCustomNotification, PendingIntent.FLAG_MUTABLE)
+        val notificationLayout = RemoteViews(packageName, R.layout.activity_collapse)
+        val notificationLayoutExpanded = RemoteViews(packageName, R.layout.activity_expanded)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = NotificationCompat.Builder(this, channelIDFullSCNot)
+                .setSmallIcon(R.drawable.ic_call)
+                .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                .setCustomContentView(notificationLayout)
+                .setCustomBigContentView(notificationLayoutExpanded)
+                .setContentIntent(fullyCustomNotificationPending)
+                .setPriority(NotificationCompat.DEFAULT_LIGHTS)
+                .setAutoCancel(true)
+            channel = NotificationChannel(
+                channelIDFullSCNot,
+                "Custom Notification",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Custom Notification"
+            }
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(3, builder.build())
+
+    }
+
+    private fun fullScreenNotification() {
+        val fullScreen = Intent(this, MainActivity::class.java)
+        val fullScreenPendingIntent =
+            PendingIntent.getActivity(this, 2, fullScreen, PendingIntent.FLAG_MUTABLE)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = NotificationCompat.Builder(this, channelIDFullSNot)
+                .setSmallIcon(R.drawable.ic_call)
+                .setContentTitle("Miss Call")
+                .setContentText("+91 1234567890")
+                .setCategory(NotificationCompat.CATEGORY_CALL)
+                .setContentIntent(fullScreenPendingIntent)
+                .setFullScreenIntent(fullScreenPendingIntent, true)
+                .setAutoCancel(true)
+            channel = NotificationChannel(
+                channelIDFullSNot,
+                "Reminder",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Good Morning"
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(2, builder.build())
+    }
+
+
+
+    private fun normalNotification() {
+        val bitmapImage = BitmapFactory.decodeResource(resources, R.drawable.imgavatar)
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_MUTABLE)
+        val bigStyleText =
+            "hello hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = NotificationCompat.Builder(this, channelIDNormalNot)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Alert")
+                .setContentText("Hello how are You ?")
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(bigStyleText)
+                )
+                .setLargeIcon(bitmapImage)
+                .setStyle(
+                    NotificationCompat.BigPictureStyle()
+                        .bigPicture(bitmapImage)
+                        .bigLargeIcon(null)
+                )
+
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+            channel = NotificationChannel(
+                channelIDNormalNot, getString(R.string.app_name), NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Hello Every One"
+            }
+            notificationManager.createNotificationChannel(channel)
+        } else {
+            builder = NotificationCompat.Builder(this, channelIDNormalNot)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Alert")
+                .setContentText("Hello how are You ?")
+                .setLargeIcon(bitmapImage)
+                .setStyle(
+                    NotificationCompat.BigPictureStyle().bigPicture(bitmapImage).bigLargeIcon(null)
+                )
+                .setPriority(NotificationCompat.PRIORITY_MIN)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        }
+        notificationManager.notify(1, builder.build())
+    }
+
 
 }
+
+
